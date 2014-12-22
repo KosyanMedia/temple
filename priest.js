77005d329cf0c03155424ad26d426917f15e8937
bskaplou <eshikov@mail.ru>
2014-12-23 05:24:23 +0700

    initial commit of template engine

---
+++ priest.js
@@ -0,0 +1,143 @@
+#!/home/bskaplou/bin/node
+var DOMParser = require('xmldom').DOMParser;
+
+var lid = 0;
+function new_id() {
+    return lid++;
+}
+
+function node(template_id, parent_id, n, emit) {
+    function attribute(name, value, parent_id) {
+        if(value.indexOf('{{') != -1) {
+            var chunks = value.split('{{');
+            chunks[0].length && emit('attr', template_id, parent_id, name, ['C', chunks[0]]);
+            for(var i = 1, l = chunks.length, c = chunks[1]; i < l; i++, c = chunks[i]) {
+                var _ = c.split('}}', 2);
+                var key = _[0], text = _[1];
+                emit('attr', template_id, parent_id, name, ['V', key]);
+                text.length && emit('attr', template_id, parent_id, name, ['C', text]);
+            }
+        } else {
+            emit('attr', template_id, parent_id, name, ['C', value]);
+        }
+    }
+
+    function string(value) {
+        if(value.indexOf('{{') != -1) {
+            var chunks = value.split('{{');
+            chunks[0].length && emit('text', template_id, parent_id, ['C', chunks[0]]);
+            for(var i = 1, l = chunks.length, c = chunks[1]; i < l; i++, c = chunks[i]) {
+                var _ = c.split('}}', 2);
+                var key = _[0], text = _[1];
+                emit('text', template_id, parent_id, ['V', key]);
+                text.length && emit('text', template_id, parent_id, ['C', text]);
+            }
+        } else {
+            emit('text', template_id, parent_id, ['C', value]);
+        }
+    }
+
+    if(n.nodeType == 9) { //Document
+        node(template_id, parent_id, n.firstChild, emit);
+    } else if(n.nodeType == 3) { //Text
+        string(n.nodeValue);
+    } else if(n.nodeType == 1) { //Element
+        if(n.tagName == 'forall') {
+            var new_template_id = template_id + '_nested' + new_id();
+            emit('forall', template_id, parent_id, n.getAttribute('key'), new_template_id);
+            for(var i = 0, c = n.childNodes, l = n.childNodes.length; i < l; i++) {
+                 node(new_template_id, 'root', c[i], emit);
+            }
+        } else {
+            var node_id = 'node' + new_id();
+            emit('node', template_id, n.tagName, node_id);
+            emit('link', template_id, parent_id, node_id);
+            if(n.attributes) {
+                for(var a = n.attributes, i = 0, l = n.attributes.length; i < l; i++) {
+                    attribute(a[i].name, a[i].value, node_id);
+                }
+            }
+            if(n.childNodes) {
+                for(var i = 0, c = n.childNodes, l = n.childNodes.length; i < l; i++) {
+                    node(template_id, node_id, c[i], emit);
+                }
+            }
+        }
+    } else {
+        console.log('||nodeType=>' + n.nodeType);
+    }
+}
+
+var templates = {};
+function collector(ins, source, arg1, arg2, arg3) {
+    if(! templates.hasOwnProperty(source)) {
+        templates[source] = [];
+    }
+    if(ins == 'node') { //Nodes go up
+        templates[source].unshift([ins, arg1, arg2]);
+    } else { // Other down
+        templates[source].push([ins, arg1, arg2, arg3]);
+    }
+}
+
+function builder(instructions) {//Single-pass translator
+    function esc(s) {
+        return s.replace(/"/g, '\\"').replace(/\n/g, '\\n');
+    }
+
+    var declarations = ['var root_node = document.createDocumentFragment();'];
+    var links = [];//Order is important here
+    var accessors = [];
+    for(var i = 0, l = instructions.length; i < l; i++) {
+        var ins = instructions[i];
+        if(ins[0] == 'attr') {
+            if(ins[3][0] == 'V') {
+                declarations.push('var ' + ins[3][1] + '_attr = document.createAttribute("' + ins[2] + '");');
+                links.unshift(ins[1] + '_node.setAttributeNode(' + ins[3][1] + '_attr);');
+                accessors.push(ins[3][1] + ': function(value){if(value){' + ins[3][1] + '_attr.value = value; } else {return ' + ins[3][1] + '_attr.value;}}');
+            } else if(ins[3][0] == 'C') {
+                links.unshift(ins[1] + '_node.setAttribute("' + ins[2] + '", "' + esc(ins[3][1]) + '");');
+            }
+        } else if(ins[0] == 'text') {
+            if(ins[2][0] == 'V') {
+                declarations.push('var ' + ins[2][1] + '_text = document.createTextNode("");');
+                links.push(ins[1] + '_node.appendChild(' + ins[2][1] + '_text);');
+                accessors.push(ins[2][1] + ': function(value){if(value){' + ins[2][1] + '_text.nodeValue = value; } else {return ' + ins[2][1] + '_node.nodeValue;}}');
+            } else if(ins[2][0] == 'C') {
+                links.push(ins[1] + '_node.appendChild(document.createTextNode("' + esc(ins[2][1]) + '"));');
+            }
+        } else if(ins[0] == 'node') {
+            declarations.push('var ' + ins[2] + '_node = document.createElement("' + ins[1] + '");' );
+        } else if(ins[0] == 'link') {
+            links.push(ins[1] + '_node.appendChild(' + ins[2] + '_node);');
+        } else if(ins[0] == 'forall') {
+            declarations.push('var before_' + ins[3] + ' = document.createTextNode("");');
+            declarations.push('var after_' + ins[3] + ' = document.createTextNode("");');
+            links.push(ins[1] + '_node.appendChild(before_' + ins[3] + ');');
+            links.push(ins[1] + '_node.appendChild(after_' + ins[3] + ');');
+            accessors.push(ins[2] + ': ' + 'function(value){render_template(before_' + ins[3] + ', after_' + ins[3] + ', "' + ins[3] + '", value, pool);}');
+        }
+    }
+    var accessors_code = '{' + accessors.join(', ')+ '}';
+    links.push('return [root_node, ' + accessors_code + '];');
+    return declarations.join('\n') + '\n' + links.join('\n');
+}
+
+var fs = require('fs');
+var parser = new DOMParser();
+process.argv.forEach(function(val, index, array) {
+  if(index > 1) {
+      var _ = val.split('.');
+      _.pop();
+      _ = _.join('.').split('/');
+      var name = _.pop();
+      var template_string =  fs.readFileSync(val, {encoding: 'utf8'});
+      node(name, 'root', parser.parseFromString(template_string), collector);
+  }
+});
+
+var templates_code = [];
+for(var k in templates) {
+    templates_code.push(k + ': function(pool){' + builder(templates[k]).replace(/\n/g, "\n        ") + '\n    }');
+}
+console.log('var templates_list = {\n    ' + templates_code.join(',\n    ') + '\n};')
---
+++ utils.js
@@ -0,0 +1,82 @@
+function render_template(before, after, template, data, pool) {
+    var parent = before.parentNode;
+    while(before.nextSibling != after) {
+        parent.removeChild(before.nextSibling);
+    }
+    var fragment = document.createDocumentFragment();
+    for(var i = 0, l = data.length; i < l; i++) {
+        var nested = pool.get(template);
+        fragment.appendChild(nested[0]);
+        var d = data[i];
+        for(var k in d) {
+            if(nested[1].hasOwnProperty(k)) {
+                nested[1][k](d[k]);
+            }
+        }
+    }
+    parent.insertBefore(fragment, after);
+}
+
+function pool(templates) {
+    var busy = {};
+    var free = {};
+    var methods = {
+        info: function() {
+            var tor = {free: {}, busy: {}}, bkeys = Object.keys(busy), fkeys = Object.keys(free);
+            for(var i = 0, l = bkeys.length; i < l; i++) {
+                var k = bkeys[i];
+                tor.busy[k] = busy[k].length;
+            }
+            for(var i = 0, l = fkeys.length; i < l; i++) {
+                var k = fkeys[i];
+                tor.free[k] = free[k].length;
+            }
+            return tor;
+        },
+        release: function() {
+            var keys = Object.keys(busy);
+            for(var i = 0, l = keys.length; i < l; i++) {
+                var key = keys[i];
+                if(! free.hasOwnProperty(key)) {
+                    free[key] = [];
+                }
+                free[key] = free[key].concat(busy[key]);
+                busy[key] = [];
+            }
+        },
+        build_cache: function(to_cache) {
+            var keys = Object.keys(to_cache);
+            for(var i = 0, l = keys.length; i < l; i++) {
+                var key = keys[i];
+                var arr;
+                if(! free.hasOwnProperty(key)) {
+                    arr = [];
+                    free[key] =  arr;
+                } else {
+                    arr = free[key];
+                }
+
+                for(var j = 0, k = to_cache[key]; j < k; j++) {
+                    arr.push(templates[key](methods));
+                }
+            }
+            console.log(free);
+        },
+        get: function(template) {
+            if(! busy.hasOwnProperty(template)) {
+                busy[template] = [];
+            }
+            var tor;
+            if(free.hasOwnProperty(template) && free[template].length > 0) {
+                tor = free[template].pop();
+            } else {
+                tor = templates[template](methods);
+            }
+            busy[template].push(tor);
+            return tor;
+        }
+    };
+    return methods;
+}
+
+window.templates_pool = pool(templates_list);
