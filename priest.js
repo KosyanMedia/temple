92ca305bbb15977d31c1b07329a080a48c5c3d16
Alexander Zhukov <al.zhukoff@gmail.com>
2014-12-26 14:17:43 +0700

    fix

--- priest.js
+++ priest.js
@@ -126,18 +126,18 @@
       for(var i = 0, l = instructions.length; i < l; i++) {
         var ins = instructions[i];
         if(ins[0] == 'attr') {
-          if(!declarated_attributes[ins[2]]){
-            declarated_attributes[ins[2]] = true;
-            declarations.push('var ' + ins[2] + '_attr = document.createAttribute("' + ins[2] + '");');
+          if(!declarated_attributes[ins[2] + i]){
+            declarated_attributes[ins[2] + i] = true;
+            declarations.push('var ' + ins[2] + i + '_attr = document.createAttribute("' + ins[2] + '");');
           }
           if(ins[3][0] == 'V') {
             var variable = split_var(ins[3][1]);
             declarations.push('var ' + variable[0] + '_attr = document.createTextNode("");');
-            links.push(ins[2] + '_attr.appendChild(' + variable[0] + '_attr)');
+            links.push(ins[2] + i + '_attr.appendChild(' + variable[0] + '_attr)');
             links.unshift(ins[1] + '_node.setAttributeNode(' + ins[2] + '_attr);');
             accessors.push(variable[0] + ': function(value){' + variable[0] + '_attr.nodeValue = ' + variable[2] + '(value' + variable[1] + ');}');
           } else if(ins[3][0] == 'C') {
-            links.push(ins[2] + '_attr.appendChild(document.createTextNode("' + esc(ins[3][1]) + '"))');
+            links.push(ins[2] + i + '_attr.appendChild(document.createTextNode("' + esc(ins[3][1]) + '"))');
           }
         } else if(ins[0] == 'text') {
           if(ins[2][0] == 'V') {
