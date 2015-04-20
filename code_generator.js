35e922014823cd73218a3d0a0946bd089542ef48
bskaplou <eshikov@mail.ru>
2015-04-20 12:14:48 +0700

    refactoring(all): code splitted between files

---
+++ code_generator.js
@@ -0,0 +1,294 @@
+(function(module) {
+    var lid = 0;
+    function new_id() {
+      return lid++;
+    }
+
+   function split_var(v, pid) {
+      var v = v.replace(/^\s+|\s+$/g, '');
+      var pipe = v.indexOf('|');
+      var dot = v.indexOf('.');
+      var bra = v.indexOf('[');
+      var tor = [v, '', '', pid + '_' + v.replace(/\.|\[|\]/g, '_')];
+      if(pipe != -1) {
+        tor[2] = 'filters.' + v.substr(pipe + 1).replace(/^\s+|\s+$/g, '');
+        v = v.substr(0, pipe).replace(/^\s+|\s+$/g, '');
+        tor[3] = pid + '_' + v.replace(/\.|\[|\]/g, '_');
+      }
+      if(dot != -1 || bra != -1) {
+        if(dot == - 1)
+          dot = 1000;
+        if(bra == - 1)
+          bra = 1000;
+        var ind = Math.min(dot, bra);
+        tor[0] = v.substr(0, ind);
+        tor[1] = v.substr(ind);
+      } else {
+        tor[0] = v;
+        tor[1] = '';
+      }
+      return tor;
+    }
+
+    module.exports = function(instructions) {//Single-pass translator enchanted with buffered optimizations!
+      function esc(s) {
+        return s.replace(/"/g, '\\"').replace(/\n/g, '\\n');
+      }
+      var variables = {};
+      var root = 'root';
+      var add_variable = function(name, type) {
+        if(!(name in variables)) {
+          variables[name] = [];
+        }
+        variables[name].push(type);
+      }
+      var root_children = [];
+      var buff = [];
+
+      var declarations = [],
+          links = [], // Order is important here
+          accessors = {remove: []};
+
+      for(var i = 0, l = instructions.length; i < l; i++) {
+        var ins = instructions[i],
+            instruction = ins[0],
+            parent_id = ins[1];
+
+        if(buff.length > 0 && buff[0][0] == 'text' && (instruction != 'text' || buff[0][1] != parent_id)) {
+          if(buff.length == 1) {
+            var value_type = buff[0][2][0], // Variable or Constant
+                value = buff[0][2][1],
+                pid = buff[0][1];
+  
+            if(value_type == 'V') { // Variable
+              var variable = split_var(value, pid);
+              add_variable(variable[0], 'text');
+              var var_id = variable[3] + new_id() + '_text';
+              declarations.push('var ' + var_id + ' = document.createTextNode("");');
+              if(buff[0][1] == 'root') {
+                accessors.remove.push(var_id + '.parentNode.removeChild(' + var_id + ');'); 
+                root_children.push(buff[0][1] + '.appendChild(' + var_id + ');');
+              } else {
+                links.push(buff[0][1] + '.appendChild(' + var_id + ');');
+              }
+              accessors[variable[0]] = accessors[variable[0]] || [];
+              accessors[variable[0]].push(var_id + '.nodeValue = ' + variable[2] + ' (value' + variable[1] + ')');
+            } else if(value_type == 'C') { // Constant
+              if(buff[0][1] == 'root') {
+                var node_var_name = 'root_text' + new_id();
+                declarations.push('var ' + node_var_name + ' = document.createTextNode("' + esc(value) + '");');
+                accessors.remove.push(node_var_name + '.parentNode.removeChild(' + node_var_name + ');'); 
+                root_children.push(buff[0][1] + '.appendChild(' + node_var_name + ');');
+                root_children.push(' ');
+              } else {
+                links.push(buff[0][1] + '.appendChild(document.createTextNode("' + esc(value) + '"));');
+              }
+            }
+          } else {
+            var tid = new_id();
+            var node_var_name = buff[0][1] + '_text' + tid;
+            if(buff[0][1] == 'root') {
+              accessors.remove.push(node_var_name + '.parentNode.removeChild(' + node_var_name + ');'); 
+              root_children.push(buff[0][1] + '.appendChild(' + node_var_name + ');');
+              root_children.push(' ');
+            } else {
+              links.push(buff[0][1] + '.appendChild(' + node_var_name + ');');
+            }
+            var parts = [];
+            var const_parts = [];
+            var access_keys = [];
+            var vars_count = 0;
+            for(var j = 0, k = buff.length; j < k; j++) {
+              if(buff[j][2][0] == 'V')
+                vars_count++;
+            }
+            for(var j = 0, k = buff.length; j < k; j++) {
+              var pid = buff[j][1],
+                  value_type = buff[j][2][0], // Variable or Constant
+                  value = buff[j][2][1];
+
+              if(value_type == 'C') {
+                parts.push('"' + esc(value) + '"');
+                const_parts.push('"' + esc(value) + '"');
+              } else if(value_type == 'V') {
+                var variable = split_var(value, pid);
+                accessors[variable[0]] = accessors[variable[0]] || [];
+                var var_id = variable[0] + new_id() + '_var';
+                add_variable(variable[0], 'text');
+                access_keys.push(variable[0]);
+                if(vars_count > 1) {
+                  parts.push(var_id);
+                  declarations.push('var ' + var_id + ' = "";');
+                  accessors[variable[0]].push(var_id + ' = ' + variable[2] + '(value' + variable[1] + ')');
+                } else {
+                  parts.push(variable[2] + '(value' + variable[1] + ')');
+                }
+              }
+            }
+            text_update_code = node_var_name + '.nodeValue = ' + parts.join(' + ');
+            while(access_keys.length) {
+              var v = access_keys.pop();
+              if(accessors[v].indexOf(text_update_code) >= 0) {
+                accessors[v].splice(accessors[v].indexOf(text_update_code), 1);
+              }
+              accessors[v].push(text_update_code);
+            }
+            declarations.push('var ' + node_var_name + ' = document.createTextNode(' + const_parts.join('+').replace(/"\+"/g, "") + ');');
+          }
+          buff = [];
+        }
+
+        if(buff.length > 0 && buff[0][0] == 'attr' && (instruction != 'attr' || buff[0][1] != parent_id || buff[0][2] != ins[2])) {
+          if(buff.length == 1) {
+            var pid = buff[0][1],
+                attr = buff[0][2],
+                value_type = buff[0][3][0], // Variable or Constant
+                value = buff[0][3][1];
+
+            if(value_type == 'V') { // Variable
+              var variable = split_var(value, pid);
+              add_variable(variable[0], 'attr');
+              accessors[variable[0]] = accessors[variable[0]] || [];
+              if(attr == 'value' || attr == 'checked') {
+                accessors[variable[0]].push(pid + '.' + attr + ' = ' + variable[2] + '(value' + variable[1] + ')');
+              } else {
+                accessors[variable[0]].push(pid + '.setAttribute("' + attr + '", ' + variable[2] + '(value' + variable[1] + '))');
+              }
+            } else if(value_type == 'C') { // Constant
+              links.unshift(pid + '.setAttribute("' + attr + '", "' + esc(value) + '");');
+            }
+          } else {
+            var const_parts = [];
+            var parts = [];
+            var node_var_name = buff[0][1];
+            var access_keys = [];
+            var vars_count = 0;
+            for(var j = 0, k = buff.length; j < k; j++) {
+              if(buff[j][3][0] == 'V')
+                vars_count++;
+            }
+            for(var j = 0, k = buff.length; j < k; j++) {
+              var pid = buff[j][1],
+                  attr = buff[j][2],
+                  value_type = buff[j][3][0], // Variable or Constant
+                  value = buff[j][3][1];
+
+              if(value_type == 'C') {
+                parts.push('"' + esc(value) + '"');
+                const_parts.push('"' + esc(value) + '"');
+              } else if(value_type == 'V') {
+                var variable = split_var(value, pid);
+                var var_id = variable[0] + new_id() + '_var';
+                access_keys.push(variable[0]);
+                add_variable(variable[0], 'attr');
+                accessors[variable[0]] = accessors[variable[0]] || [];
+                if(vars_count > 1) {
+                  parts.push(var_id);
+                  declarations.push('var ' + var_id + ' = "";');
+                  accessors[variable[0]].push(var_id + ' = ' + variable[2] + '(value' + variable[1] + ')');
+                } else {
+                  parts.push(variable[2] + '(value' + variable[1] + ')');
+                }
+              }
+            }
+            var attr_update_code;
+            var attr_set_code = false;
+            if(buff[0][2] == 'value' || buff[0][2] == 'checked') {
+              attr_update_code =  node_var_name + '.' + buff[0][2] + ' = ' + parts.join('+');
+              attr_set_code =  node_var_name + '.' + buff[0][2] + ' = ' + const_parts.join(' + ').replace(/"\+"/g, "");
+            } else {
+              attr_update_code =  node_var_name + '.setAttribute("' + buff[0][2] + '",' + parts.join('+') + ')';
+              if(buff[0][2] != 'src' && buff[0][2] != 'href') {
+                attr_set_code =  node_var_name + '.setAttribute("' + buff[0][2] + '",' + const_parts.join('+').replace(/"\+"/g, "") + ')';
+              }
+            }
+            while(access_keys.length > 0) {
+              var v = access_keys.pop();
+              if(accessors[v].indexOf(attr_update_code) >= 0) {
+                accessors[v].splice(accessors[v].indexOf(attr_update_code), 1);
+              }
+              accessors[v].push(attr_update_code);
+            }
+            if(attr_set_code != false)
+              declarations.push(attr_set_code + ';');
+          }
+          buff = [];
+        }
+
+        if(instruction == 'attr' || instruction == 'text') {
+          buff.push(ins);
+        } else if(instruction == 'node') {
+          var node = ins[2];
+
+          declarations.push('var ' + node + ' = document.createElement("' + parent_id + '");' );
+        } else if(instruction == 'link') {
+          var node = ins[2];
+          if(parent_id == 'root') {
+            root_children.push(parent_id + '.appendChild(' + node + ');');
+            root = node;
+            accessors.remove.push(node + '.parentNode.removeChild(' + node + ');'); 
+          } else {
+            links.push(parent_id + '.appendChild(' + node + ');');
+          }
+        } else if(instruction == 'if' || instruction == 'forall') {
+          var variable = split_var(ins[2], parent_id), // Accessor key
+              tpl = ins[3]; // Template to loop over
+          var tpl_id = tpl + new_id();
+          declarations.push('var child_' + tpl_id + ' = [];');
+          add_variable(variable[0], 'key');
+          declarations.push('var after_' + tpl_id + ' = document.createTextNode("");');
+          if(parent_id == 'root') {
+            root_children.push(parent_id + '.appendChild(after_' + tpl_id + ');');
+            accessors.remove.push('after_' + tpl_id + '.parentNode.removeChild(after_' + tpl_id + ');');
+            accessors.remove.unshift('while(child_' + tpl_id + '.length) pool.release("' + tpl + '", child_' + tpl_id + '.pop());');
+          } else {
+            links.push(parent_id + '.appendChild(after_' + tpl_id + ');');
+          }
+          accessors[variable[0]] = accessors[variable[0]] || [];
+          if(instruction == 'if') {
+            accessors[variable[0]].push('temple_utils.render_child(after_' + tpl_id + ', "' + tpl + '", ' + variable[2]+ ' (value' + variable[1] + '), pool, child_' + tpl_id + ')');
+          } else if(instruction == 'forall') {
+            accessors[variable[0]].push('temple_utils.render_children(after_' + tpl_id + ', "' + tpl + '", ' + variable[2] + ' (value' + variable[1] + '), pool, child_' + tpl_id + ')');
+          }
+        }
+      }
+      var accessors_code = [];
+      if(Object.keys(accessors).length > 0) {
+        accessors_code.push('{');
+        accessors['update'] = [];
+        for(var key in variables) {
+          accessors['update'].push('t = value.' + key);
+          accessors['update'].push('if(typeof t !== \'undefined\') this.' + key + '(t)');
+        }
+        if(accessors['update'].length > 0) {
+          accessors['update'][0] = 'var ' + accessors['update'][0];
+        }
+        for(var key in accessors) {
+          if(key == 'remove' || key == 'root') {
+            accessors_code.push(key + ':function(){' );
+          } else {
+            accessors_code.push(key + ':function(value){' );
+          }
+          accessors_code.push(accessors[key].join(';'));
+          accessors_code.push('}');
+          accessors_code.push(',');
+        }
+        accessors_code.pop();
+	accessors_code.push('}');
+      } else {
+	accessors_code.push('{}');
+      }
+      if(root_children.length == 1) {
+        accessors_code.pop();
+        accessors_code.push(',root: function(){return ' + root + ';}');
+	accessors_code.push('}');
+      } else {
+        root = 'root';
+        accessors_code.pop();
+        accessors_code.push(',root: function(){var root = document.createDocumentFragment();' + root_children.join('') + 'return root;}');
+	accessors_code.push('}');
+      }
+      links.push('return ' + accessors_code.join('') + ';');
+      return declarations.join('') + links.join('');
+    }
+})(module); 
---
+++ xml2instructions.js
@@ -0,0 +1,74 @@
+(function(module){
+    var lid = 0;
+    function new_id() {
+      return lid++;
+    }
+
+    function node(template_id, parent_id, n, emit) {
+      function attribute(name, value, parent_id) {
+        if(value.indexOf('{{') != -1) {
+          var chunks = value.split('{{');
+          chunks[0].length && emit('attr', template_id, parent_id, name, ['C', chunks[0]]);
+          for(var i = 1, l = chunks.length, c = chunks[1]; i < l; i++, c = chunks[i]) {
+            var _ = c.split('}}', 2);
+            var key = _[0], text = _[1];
+            emit('attr', template_id, parent_id, name, ['V', key]);
+            text.length && emit('attr', template_id, parent_id, name, ['C', text]);
+          }
+        } else {
+          emit('attr', template_id, parent_id, name, ['C', value]);
+        }
+      }
+
+      function string(value) {
+        if(value.indexOf('{{') != -1) {
+          var chunks = value.split('{{');
+          chunks[0].length && emit('text', template_id, parent_id, ['C', chunks[0]]);
+          for(var i = 1, l = chunks.length, c = chunks[1]; i < l; i++, c = chunks[i]) {
+            var _ = c.split('}}', 2);
+            var key = _[0], text = _[1];
+            emit('text', template_id, parent_id, ['V', key]);
+            text.length && emit('text', template_id, parent_id, ['C', text]);
+          }
+        } else {
+          emit('text', template_id, parent_id, ['C', value]);
+        }
+      }
+
+      if(n.nodeType == 9) { //Document
+        node(template_id, parent_id, n.firstChild, emit);
+        emit('stop', template_id);
+      } else if(n.nodeType == 3) { //Text
+        string(n.nodeValue);
+      } else if(n.nodeType == 1) { //Element
+        if(n.tagName == 'forall' || n.tagName == 'if') {
+          var new_template_id = template_id + '_' + n.tagName +'_' + new_id();
+          if(n.hasAttribute('name')) {
+            new_template_id = n.getAttribute('name');
+          }
+          emit(n.tagName, template_id, parent_id, n.getAttribute('key'), new_template_id);
+          for(var i = 0, c = n.childNodes, l = n.childNodes.length; i < l; i++) {
+            node(new_template_id, 'root', c[i], emit);
+          }
+          emit('stop', new_template_id);
+        } else {
+          var node_id = 'n' + new_id();
+          emit('node', template_id, n.tagName, node_id);
+          if(n.attributes) {
+            for(var a = n.attributes, i = 0, l = n.attributes.length; i < l; i++) {
+              attribute(a[i].name, a[i].value, node_id);
+            }
+          }
+          if(n.childNodes) {
+            for(var i = 0, c = n.childNodes, l = n.childNodes.length; i < l; i++) {
+              node(template_id, node_id, c[i], emit);
+            }
+          }
+          emit('link', template_id, parent_id, node_id);
+        }
+      } else {
+        //console.log('||nodeType=>' + n.nodeType);
+      }
+    }
+    module.exports = node;
+})(module);
