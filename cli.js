756376ee84e8d0a15d62500388ea0a0426106c17
fliptheweb <itisshortemailaddress@gmail.com>
2015-05-04 22:39:16 +0700

    feat(cli.js) - add help to output, set script env on top

--- cli.js
+++ cli.js
@@ -1,8 +1,17 @@
+#!/usr/bin/env node
 var temple = require('./priest');
 var template;
-if(process.argv.slice(-1)[0] === "-m"){
-  template = temple(process.argv.slice(2, -1), true);
+var args = process.argv.slice(2);
+if(args.length == 0 || args.indexOf('-h') != -1 || args.indexOf('--help') != -1) {
+  console.log("Usage: cli.js [ template.temple ] [arguments]");
+  console.log("       cli.js template.temple -m\n");
+  console.log("Arguments:");
+  console.log("  -m   build as module")
 } else {
-  template = temple(process.argv.slice(2), false, true);
+  if(args.indexOf('-m') != -1) {
+    template = temple(args.slice(0, -1), true);
+  } else {
+    template = temple(args, false, true);
+  }
+  console.log(template);
 }
-console.log(template);
